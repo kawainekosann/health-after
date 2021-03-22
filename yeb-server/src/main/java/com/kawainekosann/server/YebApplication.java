c84ca27d43559c709e22bddb839356199d4bf89c
LIU <1532921016@qq.com>
2021-03-22 17:46:22 +0800

    commit

--- yeb-server/src/main/java/com/kawainekosann/server/YebApplication.java
+++ yeb-server/src/main/java/com/kawainekosann/server/YebApplication.java
@@ -1,11 +1,15 @@
 package com.kawainekosann.server;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 
 /**
  * 启动类s
  */
+@PropertySource(value = "classpath:config/jdbc.properties")
 @SpringBootApplication
 @MapperScan("com.kawainekosann.server.dao")
 public class YebApplication {
