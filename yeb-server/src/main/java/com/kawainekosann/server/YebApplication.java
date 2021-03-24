1cac9eacf710743bd27c9e8cc77acb37a9e49c55
LIU <1532921016@qq.com>
2021-03-24 17:32:53 +0800

    提交

--- yeb-server/src/main/java/com/kawainekosann/server/YebApplication.java
+++ yeb-server/src/main/java/com/kawainekosann/server/YebApplication.java
@@ -1,19 +1,23 @@
 package com.kawainekosann.server;
+
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
 
 /**
  * 启动类s
  */
 @PropertySource(value = "classpath:config/jdbc.properties")
-@SpringBootApplication
 @MapperScan("com.kawainekosann.server.dao")
+//想要改变扫描路径，@SpringBootApplication(scanBasePackages="com.atguigu")
+//或者@ComponentScan 指定扫描路径
+@SpringBootApplication
 public class YebApplication {
     public static void main(String[] args) {
         SpringApplication.run(YebApplication.class, args);
     }
 }
+
+
+
--- yeb-server/src/main/java/com/kawainekosann/server/controller/TestController.java
+++ yeb-server/src/main/java/com/kawainekosann/server/controller/TestController.java
@@ -7,9 +7,15 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//@Configuration(proxyBeanMethods = false)
+//proxyBeanMethodstrue 使用代理，直接从IOC容器之中取得对象；如果设置为false,也就是不使用注解，每次调用@Bean标注的方法获取到的对象和IOC容器中的都不一样，是一个新的对象，所以我们可以将此属性设置为false来提高性能
+
+//@Import({User.class, DBHelper.class})
+//给容器中自动创建出这两个类型的组件、默认组件的名字就是全类名
 @RestController
 //用ConstructorBinding 必须启用EnableConfigurationProperties
 @EnableConfigurationProperties(TestYaml.class)
