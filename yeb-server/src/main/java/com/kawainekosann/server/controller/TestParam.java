20d8c9c1028b035b43590c13e0090f7687a59923
LIU <1532921016@qq.com>
2021-03-26 17:45:52 +0800

    提交

--- yeb-server/src/main/java/com/kawainekosann/server/controller/TestParam.java
+++ yeb-server/src/main/java/com/kawainekosann/server/controller/TestParam.java
@@ -23,6 +23,8 @@ public class TestParam {
                                       @RequestHeader("User-Agent") String UserAgent,
                                       @RequestHeader Map<String, String> headers,
                                       @RequestParam("age") Integer age,
+                                      //获取request attribute的内容
+                                      //@RequestAttribute("msg") String msg,
                                       //@CookieValue("ga") String ga,
                                       //@CookieValue("ga") Cookie cookie
                                       //@RequestParam("intrests")List<String> intrest ,
@@ -46,4 +48,34 @@ public class TestParam {
         return map;
     }
 
+    //1、语法： 请求路径：/cars/sell;low=34;brand=byd,audi,yd
+    //2、SpringBoot默认是禁用了矩阵变量的功能
+    //      手动开启：原理。对于路径的处理。UrlPathHelper进行解析。
+    //              removeSemicolonContent（移除分号内容）支持矩阵变量的
+    //3、矩阵变量必须有url路径变量才能被解析
+    @GetMapping("/cars/{path}")
+    public Map carsSell(@MatrixVariable("low") Integer low,
+                        @MatrixVariable("brand") List<String> brand,
+                        @PathVariable("path") String path){
+        Map<String,Object> map = new HashMap<>();
+
+        map.put("low",low);
+        map.put("brand",brand);
+        map.put("path",path);
+        return map;
+    }
+
+
+    // /boss/1;age=20/2;age=10
+
+    @GetMapping("/boss/{bossId}/{empId}")
+    public Map boss(@MatrixVariable(value = "age",pathVar = "bossId") Integer bossAge,
+                    @MatrixVariable(value = "age",pathVar = "empId") Integer empAge){
+        Map<String,Object> map = new HashMap<>();
+
+        map.put("bossAge",bossAge);
+        map.put("empAge",empAge);
+        return map;
+
+    }
 }
